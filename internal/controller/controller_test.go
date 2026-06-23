582c27a5413b36e9c52060ddfe53dc5a30448f03
Fernando Dantas <fernandodantas1996@gmail.com>
2026-06-23 16:33:01 -0300

    refactor: `kase` -> `idCase`

--- internal/controller/controller_test.go
+++ internal/controller/controller_test.go
@@ -8,26 +8,27 @@ import (
 
 func TestGetIdRequestForPersistence(t *testing.T) {
 	cases := []struct {
-		kase              string
+		idCase              string
 		originalIdRequest string
 		expectedResult    string
 		isOk              bool
 	}{
 		{"1", "request:checkout:something", "something", true},
 		{"2", "something", "", false},
+		{"3", "request:something", "", false},
 	}
 	for _, tt := range cases {
-		t.Run(tt.kase, func(t *testing.T) {
+		t.Run(tt.idCase, func(t *testing.T) {
 			result, err := sut.GetIdRequestForPersistence(tt.originalIdRequest)
 			if tt.isOk {
 				if err != nil {
-					t.Fatalf("case %s: expected to be ok", tt.kase)
+					t.Fatalf("case %s: expected to be ok", tt.idCase)
 				} else if result != tt.expectedResult {
-					t.Fatalf("case %s: expected %s, got %s", tt.kase, tt.expectedResult, result)
+					t.Fatalf("case %s: expected %s, got %s", tt.idCase, tt.expectedResult, result)
 				}
 			} else {
 				if err == nil {
-					t.Fatalf("case %s: expected to be error", tt.kase)
+					t.Fatalf("case %s: expected to be error", tt.idCase)
 				}
 			}
 		})
