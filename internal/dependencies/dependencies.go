77cd30de14633e740415899850143cedf580fb84
Fernando Dantas <fernandodantas1996@gmail.com>
2026-08-28 09:43:40 -0300

    feat: added tracing to database operations

--- internal/dependencies/dependencies.go
+++ internal/dependencies/dependencies.go
@@ -3,9 +3,10 @@ package dependencies
 import (
 	"context"
 	"fmt"
-	"log"
+	"log/slog"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/fmndantas/payments/internal/db"
@@ -15,10 +16,34 @@ type Tree struct {
 	DbPool *pgxpool.Pool
 }
 
+type slogQueryTracer struct{}
+
+func (slogQueryTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
+	slog.Debug("query", "sql", data.SQL, "args", data.Args)
+	return ctx
+}
+
+func (slogQueryTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) {
+	if data.Err != nil {
+		slog.Error("query failed", "err", data.Err, "command_tag", data.CommandTag.String())
+		return
+	}
+	slog.Debug("query done", "command_tag", data.CommandTag.String(), "rows_affected", data.CommandTag.RowsAffected())
+}
+
 // FIX: the context used for the pool is not the gin.Context
 func Initialize(dbConfiguration db.DbConfiguration) (*Tree, error) {
 	ctx := context.Background()
-	pool, err := pgxpool.New(ctx, dbConfiguration.Dsn())
+
+	config, err := pgxpool.ParseConfig(dbConfiguration.Dsn())
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse pool configuration: %s", err)
+	}
+
+	config.ConnConfig.Tracer = slogQueryTracer{}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
 
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect to database: %s", err)
@@ -28,7 +53,7 @@ func Initialize(dbConfiguration db.DbConfiguration) (*Tree, error) {
 		return nil, fmt.Errorf("unable to ping database: %s", err)
 	}
 
-	log.Println("Connected to database successfully")
+	slog.Info("connected to database successfully", "dsn", dbConfiguration.Dsn())
 
 	return &Tree{
 		DbPool: pool,
